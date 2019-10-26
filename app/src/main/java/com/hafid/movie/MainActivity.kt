a8590bf5b97663bbfd16b892787acc1468cb9b79
Muhamad Alam Syah <55674991+alamsyahh15@users.noreply.github.com>
2019-10-26 23:52:12 +0700

    MainActivity.kt
    Ada kekurangan dan kelebihan mas di bagian yang saya comment

--- app/src/main/java/com/hafid/movie/MainActivity.kt
+++ app/src/main/java/com/hafid/movie/MainActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     
-    private lateeinit var presenter : LoginPresenter
+    private lateinit var presenter : LoginPresenter   //Lalu untuk ini tadi kelebihan e nya mas
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        presenter = LoginPresenter()
+        presenter = LoginPresenter(this) //Di bagian ini kita tambahkan this untuk memanggil contextnya nanti tinggal di let aja untuk memanggil view nya
         
     }
 }
